30f99683188ec40a044e9dc46df2a5ca70336438
alester77 <113203488+alester77@users.noreply.github.com>
2023-07-27 13:18:58 -0600

    added date function and time blocks to the html

--- Develop/script.js
+++ Develop/script.js
@@ -20,4 +20,14 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  let date = new Date();
+  let dateOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  let dateString = date.toLocaleDateString('en-US', dateOptions)
+  let dateElement = document.getElementById('currentDay');
+  dateElement.innerText = dateString;
 });
